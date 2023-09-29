2d71a0030bebf4e8d17bbdce1bbf938c968cc449
richo2002 <richo2002.44@gmail.com>
2023-09-29 16:00:39 +0100

    2nd commit

--- routes/web.php
+++ routes/web.php
@@ -28,14 +28,14 @@ Route::get('/dashboard', function () {
     return view('dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
 
+Route::get('/create-super-admin-account', [MainController::class, 'createSuperAdminAccount']);
+
 
 Route::middleware('auth')->group(function () {
 
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
-
-    Route::get('/create-super-admin-account', [MainController::class, 'createSuperAdminAccount']);
 
     Route::get('/filing-plan', [MainController::class, 'filingPlan'])->name('filing.plan');
 
